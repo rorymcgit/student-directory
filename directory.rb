c50ac1f0eb14d8ba4a0597b2d696a6e432f65804
Rory McGuinness <rorymcguinness@hotmail.co.uk>
2017-01-30 12:46:48 +0000

    only prints students with names beginning with D

--- directory.rb
+++ directory.rb
@@ -24,7 +24,9 @@ end
 
 def print(students)
   students.each_with_index do |student, index|
-    puts "#{index+1}. #{student[:name]} (#{student[:cohort].capitalize} cohort)"
+    if student[:name][0].start_with?("D")
+      puts "#{index+1}. #{student[:name]} (#{student[:cohort].capitalize} cohort)"
+    end
   end
 end
 
